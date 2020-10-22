7f82ae94a253f39af0a38d6c9b28ce817945dbbd
Rachel Brown <rachelcbrown13@gmail.com>
2020-10-22 05:57:45 -0500

    Updates

---
+++ precache-manifest.31e94608bd305878a1e8b30593c02a22.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fad92058eab20b64817d44a977c48bee",
+    "url": "/thisismefromatozReact/index.html"
+  },
+  {
+    "revision": "7daf5a5093e69932fb4a",
+    "url": "/thisismefromatozReact/static/css/2.d31ed79e.chunk.css"
+  },
+  {
+    "revision": "86617089f6735e6b4b8f",
+    "url": "/thisismefromatozReact/static/css/main.950c972b.chunk.css"
+  },
+  {
+    "revision": "7daf5a5093e69932fb4a",
+    "url": "/thisismefromatozReact/static/js/2.af5ad56b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/thisismefromatozReact/static/js/2.af5ad56b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86617089f6735e6b4b8f",
+    "url": "/thisismefromatozReact/static/js/main.e3ba99cf.chunk.js"
+  },
+  {
+    "revision": "609143a892e4c184fdfb",
+    "url": "/thisismefromatozReact/static/js/runtime-main.02b85e06.js"
+  },
+  {
+    "revision": "85fc6252d8ff9b91e638ad494f4b22bf",
+    "url": "/thisismefromatozReact/static/media/ActivityBookFrontCover.85fc6252.png"
+  },
+  {
+    "revision": "db2e019eb856184bccc255ee57d86690",
+    "url": "/thisismefromatozReact/static/media/FrontCover.db2e019e.png"
+  },
+  {
+    "revision": "c397d75d9de6657bc0701d1573886a28",
+    "url": "/thisismefromatozReact/static/media/Letter_A.c397d75d.png"
+  },
+  {
+    "revision": "224a8ea595758de22a33d74cb4f55635",
+    "url": "/thisismefromatozReact/static/media/Letter_B.224a8ea5.png"
+  },
+  {
+    "revision": "8c0cb1a6dad0445ad7933121f1699157",
+    "url": "/thisismefromatozReact/static/media/Letter_C.8c0cb1a6.png"
+  },
+  {
+    "revision": "a35e6a2e824b5c749bc111ea7bfa1331",
+    "url": "/thisismefromatozReact/static/media/Letter_D.a35e6a2e.png"
+  },
+  {
+    "revision": "0678c7a93c4eb6a14a015f27512719f4",
+    "url": "/thisismefromatozReact/static/media/Letter_E.0678c7a9.png"
+  },
+  {
+    "revision": "a9f7547bf1c2446fffecb3c57e7d5421",
+    "url": "/thisismefromatozReact/static/media/Letter_F.a9f7547b.png"
+  },
+  {
+    "revision": "f429e3a2460bc4194df2bd519a6bb1f1",
+    "url": "/thisismefromatozReact/static/media/Letter_G.f429e3a2.png"
+  },
+  {
+    "revision": "bd5a46b9b708de96843980b08275bd3d",
+    "url": "/thisismefromatozReact/static/media/Letter_H.bd5a46b9.png"
+  },
+  {
+    "revision": "dc6c0e99b756d4ca48b5bf08d81720a1",
+    "url": "/thisismefromatozReact/static/media/Letter_I.dc6c0e99.png"
+  },
+  {
+    "revision": "c6fa4c024803e12cfdc22d435423d221",
+    "url": "/thisismefromatozReact/static/media/Letter_J.c6fa4c02.png"
+  },
+  {
+    "revision": "61c8d137436d8ba2cbfb0674077c030e",
+    "url": "/thisismefromatozReact/static/media/Letter_K.61c8d137.png"
+  },
+  {
+    "revision": "6839c3f5478e0d5cc00c138441669590",
+    "url": "/thisismefromatozReact/static/media/Letter_L.6839c3f5.png"
+  },
+  {
+    "revision": "5621f79ded6b20191c88efd82770daa4",
+    "url": "/thisismefromatozReact/static/media/Letter_M.5621f79d.png"
+  },
+  {
+    "revision": "f20338c34d7b0f6de9f5ae62d328f153",
+    "url": "/thisismefromatozReact/static/media/Letter_N.f20338c3.png"
+  },
+  {
+    "revision": "e49e29cffe7bc84b3102b12caf031eb8",
+    "url": "/thisismefromatozReact/static/media/Letter_O.e49e29cf.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/thisismefromatozReact/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/thisismefromatozReact/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/thisismefromatozReact/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/thisismefromatozReact/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/thisismefromatozReact/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "fa1bf2f6e06f511e352dd28449e93abb",
+    "url": "/thisismefromatozReact/static/media/headshot.fa1bf2f6.jpg"
+  }
+]);
